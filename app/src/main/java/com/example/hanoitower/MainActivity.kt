f5be641589661556bd94799708b9c810c3fcc6f8
yevhenverkhalantsev <yve001@uit.no>
2023-01-31 15:32:18 +0100

    Har begynt med Layout activity og har settet id for variables. Initiliasert ved id.

--- app/src/main/java/com/example/hanoitower/MainActivity.kt
+++ app/src/main/java/com/example/hanoitower/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.hanoitower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myOrangeOval: View
+    private lateinit var myBlueOval: View
+    private lateinit var myRedOval: View
+    private lateinit var spendTime: TextView
+    private lateinit var startAgainButt: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        myOrangeOval = this.findViewById(R.id.orangeRing)
+        myBlueOval = this.findViewById(R.id.blueRing)
+        myRedOval = this.findViewById(R.id.redRing)
+        spendTime = this.findViewById(R.id.spendTime)
+        startAgainButt = this.findViewById(R.id.startAgainButton)
+
+        startAgainButt.setOnClickListener { startAgain() }
+
+
     }
+
+    private fun startAgain() {
+        //@TODO
+    }
+
 }
